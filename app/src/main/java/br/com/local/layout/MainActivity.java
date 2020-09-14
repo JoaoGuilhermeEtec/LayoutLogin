1193675c71f2e415e1a531019e7fa87c4e328a9a
JoaoGuilhermeEtec <70458505+JoaoGuilhermeEtec@users.noreply.github.com>
2020-09-14 15:27:29 -0300

    Initial commit

---
+++ app/src/main/java/br/com/local/layout/MainActivity.java
@@ -0,0 +1,13 @@
+package br.com.local.layout;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
